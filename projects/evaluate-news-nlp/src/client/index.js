cca206690a5f61e27f4955d2249995de55bb8d88
Jon Mullingtons <jmulhern85@gmail.com>
2022-01-27 22:24:11 +0000

    setup ready

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,3 +1,9 @@
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 
 alert("Index JS here")
 console.log("Index JS!!");
