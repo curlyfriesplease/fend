2ca196a9ca7ad8726695f6b1b44b330c63cf424f
Jon Mullingtons <jmulhern85@gmail.com>
2022-01-28 17:56:19 +0000

    copying from other branch

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -4,6 +4,41 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-
-alert("Index JS here")
 console.log("Index JS!!");
+
+document
+  .getElementById("submissionButton")
+  .addEventListener("click", sendTextToServer);
+
+const sendTextToServer = async () => {
+  const textToBeSent = document.getElementById("name").value;
+  if (textToBeSent == "") {
+    alert("Please type in some text to send!");
+    return;
+  }
+  makeRequest(textToBeSent);
+};
+
+const makeRequest = async (data) => {
+  const response = await fetch("/text");
+  try {
+    console.log("Attempting to send to server");
+    serverGetResponse = await response.json();
+    console.log("GET sent to server. This is the object returned: ");
+    console.dir(serverGetResponse);
+    updateUI(serverGetResponse);
+  } catch (error) {
+    console.log("Error sending to server: ", error);
+  }
+};
+
+// Method to update the DOM with the information returned from the GET call.
+const updateUI = async (data) => {
+  try {
+    document.getElementById("results").innerHTML = data;
+    console.log("DOM has been updated with data.");
+  } catch (error) {
+    console.log("Error updating UI: ", error);
+  }
+};
+
