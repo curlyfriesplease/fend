a68005805ca134db69b5743d89154f1f24ce2ae8
Jon Mullingtons <jmulhern85@gmail.com>
2022-01-28 17:57:11 +0000

    copy from other branch

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,8 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+//console.log(`Your API key is ${process.env.API_KEY}`)
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 
 app.use(express.static('dist'))
 
@@ -20,4 +30,8 @@ app.listen(8080, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.get('/text', function (req, res) {
+    res.send("text API called, thanks a lot")
 })
